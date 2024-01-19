8c2da11fed868d90e987248d5099e081ed57d917
EvaMaeRey <evangeline.mae@gmail.com>
2024-01-18 21:36:43 -0700

    remove ggnc usage and instead use tidyverse and sf package directly

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,7 +4,7 @@
 ###### 0. Read in shape file data  #####
 
 library(sf)
-northcarolina_county_sf <- st_read(system.file("shape/nc.shp", package="sf")) |>
+northcarolina_county_sf <- sf::st_read(system.file("shape/nc.shp", package="sf")) |>
   dplyr::rename(county_name = NAME,
                 fips = FIPS)
 
@@ -13,9 +13,26 @@ usethis::use_data(northcarolina_county_sf, overwrite = TRUE)
 
 
 #### 1, create polygon reference dataframe w xmin, ymin, xmax and ymax and save
+return_st_bbox_df <- function(sf_df){
+
+  data.frame(xmin = sf::st_bbox(sf_df)[1],
+             xmax = sf::st_bbox(sf_df)[2],
+             ymin = sf::st_bbox(sf_df)[3],
+             ymax = sf::st_bbox(sf_df)[4])
+
+}
+
+
 northcarolina_county_reference <- northcarolina_county_sf |>
-  ggnc::create_geometries_reference(
-                            id_cols = c(county_name, fips))
+  dplyr::select(county_name, fips, geometry) |>
+  dplyr::mutate(bb =
+                  purrr::map(geometry, st_bbox_df)) |>
+  tidyr::unnest(bb)
+
+
+# northcarolina_county_reference <- northcarolina_county_sf |>
+#   ggnc::create_geometries_reference(
+#                             id_cols = c(county_name, fips))
 
 usethis::use_data(northcarolina_county_reference, overwrite = TRUE)
 
